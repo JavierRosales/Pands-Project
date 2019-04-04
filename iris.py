e9e6c96ff6a9050685a45aeebcef09c20220a027
David Markham <davidmarkham76@gmail.com>
2019-04-04 18:20:12 +0100

    Created Iris.py

---
+++ iris.py
@@ -0,0 +1,9 @@
+# David Markham 2019-04-01
+# Fisher Iris Data Set 
+
+# Below is my investigation and findings on the Iris Data Set. 
+
+
+import numpy as np 
+import pandas as pd
+import matplotlib as pl 
